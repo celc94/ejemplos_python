4b89e4249c6e82c29db0995e7578ff405c9a8b97
celc94 <ces94.celc@gmail.com>
2022-04-19 22:27:48 -0500

    3th commit

---
+++ ejemplos_python/cadenas.py
@@ -0,0 +1,3 @@
+#Manejo de Cadenas
+misEquiposFavoritos = "América" + " y " + "Barcelona"  
+print("Mis Equipos Favoritos son : " + misEquiposFavoritos) #Se utiliza el + para concatenar datos tipo str
---
+++ ejemplos_python/declarar_variables.py
@@ -0,0 +1,33 @@
+variable = "Hola Mundo"
+print(variable)
+print("_________________________________")
+numero1 = 4
+numero2 = 5
+suma = numero1 + numero2
+print(suma)
+print("_________________________________")
+#Tipos de datos
+#Dato Entero
+X = 5
+print(X)
+print(type(X))
+print("_________________________________")
+#Dato Cadena
+Y = "Mision TIC 2022"
+print("Y")
+print(type(Y))
+print("_________________________________")
+#Se puede agregar una pista del tipo de dato // solo es una indicación, no condiciona el tipo de dato de la variable
+Y: str= "Universidad Nacional"
+print("Y")
+print(type(Y))
+print("_________________________________")
+#Dato flotante
+Z = 10.5
+print("Z")
+print(type(Z))
+print("_________________________________")
+#Dato Booleano, es decir verdadero o falso
+ZA = False
+print(ZA)
+print(type(ZA))
