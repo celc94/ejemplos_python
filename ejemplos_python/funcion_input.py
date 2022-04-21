9b8f81c2a5e09e7a9d94a8593be6c42733f3fabe
celc94 <ces94.celc@gmail.com>
2022-04-20 22:21:30 -0500

    operadores_aritmeticos

--- ejemplos_python/funcion_input.py
+++ ejemplos_python/funcion_input.py
@@ -1,1 +1,21 @@
-#Procesar Entrada de datos de usuario
+#Procesar Entrada de datos de usuario (input, que regresa un dato tipo str)
+#Imprime el dato que escribe el usuario en consola
+resultado = input('Escribe Tú Nombre: ')
+print('Tú nombre es: ', resultado)
+print('Fin del Programa')
+print('_____________________________________________________')
+#Conversión de datos tipo STR a numéricos
+print('Programa para Ejecutar la suma de dos números')
+numero1 = int(input('Escribe el primer número: '))
+numero2 = int(input('Escribe el segundo número: '))
+#suma = int(numero1) + int(numero2)- sin envolver el input
+suma = numero1 + numero2
+print('El resultado de la suma es: ', suma)
+print('Fin del Programa')
+print('_____________________________________________________')
+#Ejercicio califica tú día - El usuario debe de calificar su día del 1 al 10
+print('Programa para calificar el día de un usuario')
+calificacion = int(input('Cómo estuvo tú día (1 al 10): '))
+print('Mi día estuvo de: ', calificacion)
+print('Fin del Programa')
+print('_____________________________________________________')
---
+++ ejemplos_python/operadores_aritmeticos.py
@@ -0,0 +1,31 @@
+#Operadores Aritméticos
+#Suma
+#Se utiliza float para poder aceptar número decimales
+print('SUMA')
+numero1 = float(input('Digite el primer número: '))
+numero2 = float(input('Digite el segundo número: '))
+suma = numero1 + numero2
+#Utilizar f string para imprimir las variables, dichas se deben de meter en paréntesis
+print(f'El valor de la suma es: {suma}')
+print('_________________________________________')
+#Resta
+print('RESTA')
+numero3 = float(input('Digite el primer número: '))
+numero4 = float(input('Digite el segundo número: '))
+resta = numero3 - numero4
+print(f'El valor de la resta es: {resta}')
+print('_________________________________________')
+#Multiplicación
+print('MULTIPLICACIÓN')
+numero5 = float(input('Digite el primer número: '))
+numero6 = float(input('Digite el segundo número: '))
+multiplicacion = numero5 * numero6
+print(f'El valor de la multiplicación es: {multiplicacion}')
+print('_________________________________________')
+#División
+print('DIVISIÓN')
+numero7 = float(input('Digite el primer número: '))
+numero8 = float(input('Digite el segundo número: '))
+division = numero7 / numero8
+print(f'El valor de la división es: {division}')
+print('FIN DEL PROGRAMA')
